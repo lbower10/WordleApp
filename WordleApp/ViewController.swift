d05d8c3500393d0b0f98f54456a9583d79e037a9
lbower10 <lbower10@vols.utk.edu>
2023-07-20 14:44:53 -0400

    Initial Commit

---
+++ WordleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordleApp
+//
+//  Created by Logan Bowers on 7/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
